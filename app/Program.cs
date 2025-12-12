79f3118b75c6671925fcbf249d459afe1c2f566e
shejogalvez <sergio.galvez@ug.uchile.cl>
2025-12-12 12:09:10 -0300

    Implement main loop

--- app/Program.cs
+++ app/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿while (true)
+{
+    Console.WriteLine("write instruction:");
+
+    var instruction = Console.ReadLine();
+    if (instruction == null)
+    {
+        Console.WriteLine("Write something!!");
+        continue;
+    }
+    instruction = instruction.Trim();
+    switch (instruction)
+    {
+    case "add":
+        Console.WriteLine("add not implemented");
+        break;
+    case "list":
+        Console.WriteLine("list not implemented");
+        break;
+    case "edit":
+        Console.WriteLine("edit not implemented");
+        break;
+    case "delete":
+        Console.WriteLine("delete not implemented");
+        break;
+    case "search":
+        Console.WriteLine("search not implemented");
+        break;
+    case "exit":
+        Console.WriteLine("exit not implemented");
+        break;
+    default:
+        Console.WriteLine("unrecognized instruction");
+        break;
+
+    };
+}
